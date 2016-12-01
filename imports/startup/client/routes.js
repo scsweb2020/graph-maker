2729bcf8b5f992e9119e1f69f3aab179fce2e252
RB <rbungard@cs.cmu.edu>
2016-12-01 10:44:58 -0500

    Naive integration of graph maker UI

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,6 +2,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '../../ui/layouts/app-body.js';
+import '../../ui/pages/app-graph-maker.js';
 import '../../ui/pages/app-not-found.js';
 
 // Index is currently set to the Meteor installation test page
@@ -9,6 +10,14 @@ FlowRouter.route('/', {
     name: 'App.home',
     action() {
         BlazeLayout.render('App_body', { top: "hello", main: "info"});
+    },
+});
+
+// Go to graph page
+FlowRouter.route('/graph', {
+    name: 'App.graphMaker',
+    action() {
+        BlazeLayout.render('App_body', { main: "App_graphMaker"});
     },
 });
 
---
+++ imports/ui/pages/app-graph-maker.js
@@ -0,0 +1,7 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './app-graph-maker.css';
+import './app-graph-maker.html';
+
+// Template.App_body.onCreated(function appBodyOnCreated() {});
