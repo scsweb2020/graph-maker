473918bb1f8cc4678647a584fc04919dd6515cb2
RB <rbungard@cs.cmu.edu>
2016-12-01 09:16:09 -0500

    Add not found route

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,14 +1,20 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
-// import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '../../ui/layouts/app-body.js';
 import '../../ui/pages/app-not-found.js';
 
-// BlazeLayout.setRoot('body');
-
+// Index is currently set to the Meteor installation test page
 FlowRouter.route('/', {
     name: 'App.home',
     action() {
         BlazeLayout.render('App_body', { top: "hello", main: "info"});
     },
 });
+
+// the App_notFound template is used for unknown routes and missing lists
+FlowRouter.notFound = {
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_notFound' });
+  },
+};
