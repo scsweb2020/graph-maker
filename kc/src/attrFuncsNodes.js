162d0ad207028dce7872947b68e9e5f6c55ddbc8
Joel Chan <chozen86@gmail.com>
2017-04-09 05:09:23 -0400

    tooltip is handy, leave it on regardless of opacity

--- kc/src/attrFuncsNodes.js
+++ kc/src/attrFuncsNodes.js
@@ -59,15 +59,15 @@ function nodeInit(selection) {
         }
       }
     })
-     .on("mouseover", function (d) { // for tooltips
-       if (d3.select(this).style('opacity') === "0") {
-                div.transition()
-                    .duration(200)
-                    .style("opacity", .9);
-                  div.html(d.name)
-                    .style("left", (d3.event.pageX) + "px")
-                    .style("top", (d3.event.pageY - 28) + "px");
-       }
+    .on("mouseover", function (d) { // for tooltips
+      //  if (d3.select(this).style('opacity') < 1) {
+          div.transition()
+              .duration(200)
+              .style("opacity", .9);
+          div.html(d.name)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+      //  }
     })
     .on("mouseout", function (d) {
       div.transition()
